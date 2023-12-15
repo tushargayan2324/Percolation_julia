3c52a5cc9432ed9463212d23e996d5db3509d3ca
tushargayan2324 <55867762+tushargayan2324@users.noreply.github.com>
2023-12-15 13:24:21 +0530

    Add files via upload

--- perc_algo_compare.jl
+++ perc_algo_compare.jl
@@ -381,7 +381,7 @@ function find_clusters_trial_test_corrected(lat)  # Best so far....1000x1000 in 
                 
         start_site = arr_sites[1]
 
-        arr_sites = deleteat!(arr_sites, 1)
+        deleteat!(arr_sites, 1)
 
         lat[start_site].cluster_id = cl_id #set cl_id of first element in cluster list
         
@@ -408,7 +408,7 @@ function find_clusters_trial_test_corrected(lat)  # Best so far....1000x1000 in 
 
                 #arr_sites = deleteat!(arr_sites, findfirst(==(obj), arr_sites))
                 # if obj in arr_sites
-                    arr_sites = deleteat!(arr_sites, findfirst(==(obj), arr_sites)) # No need for condition, we know its going to be visited single time
+                deleteat!(arr_sites, findfirst(==(obj), arr_sites)) # No need for condition, we know its going to be visited single time
                 # end
 
                 #sleep(0.5)
@@ -418,6 +418,80 @@ function find_clusters_trial_test_corrected(lat)  # Best so far....1000x1000 in 
             track = size(neighbours_set)[1]
 
             # neighbour_arr = neighbours_set
+            neighbour_arr = SVector{track,Int64}(neighbours_set)
+   
+        end
+
+    end
+    return lat
+end
+#############################################################################################
+
+function find_clusters_trial_test_allocs_fix(lat)  # Hmmmm...Idk this solved the major problem of allocs (at high lattice number). Can I do anymore optimisation?!
+
+    arr_sites = Array{Int64}([i for i=1:(size(lat)[1])])
+    cl_id = 0
+
+    while length(arr_sites) != 0 
+        cl_id += 1
+                
+        start_site = arr_sites[1]
+
+        deleteat!(arr_sites, 1)#findfirst(==(start_site), arr_sites))
+
+        lat[start_site].cluster_id = cl_id #set cl_id of first element in cluster list
+        
+        track = size(lat[start_site].neighbours)[1] #tracking the length of neighbours set using this variable 
+        
+        neighbour_arr = SVector{track,Int64}(lat[start_site].neighbours)
+
+        while track!=0      #terminate loop when can't find any viable next site for cluster
+
+            # neighbours_set = Int64[] #next neighbour
+
+            neighbours_set = zeros(Int64, 2*size(neighbour_arr)[1]+4)
+            # println((neighbour_arr))
+            iter_track::Int64 = 0
+
+            for obj in neighbour_arr
+                lat[obj].cluster_id = cl_id
+
+                
+                for nnn in lat[obj].neighbours
+                    
+                    if lat[nnn].cluster_id == 0 && !(nnn in neighbours_set)
+                        # lat[obj].cluster_id = cl_id
+                        # if !(nnn in neighbours_set)
+                            #push!(neighbours_set, nnn)
+                            
+                            # println(iter_track)
+                            iter_track += 1
+
+                            neighbours_set[iter_track] = nnn
+                            
+                            # println(neighbours_set)
+                        # end
+                    end
+                end
+
+                
+                # println(neighbours_set)
+
+                #arr_sites = deleteat!(arr_sites, findfirst(==(obj), arr_sites))
+                # if obj in arr_sites
+                # println(arr_sites, " ", obj)
+                deleteat!(arr_sites, findfirst(==(obj), arr_sites))
+                # end
+
+                # sleep(0.01)
+                # println(length(arr_sites))
+                
+            end            
+            
+
+            deleteat!(neighbours_set, neighbours_set.==0)
+            track = size(neighbours_set)[1]
+            # println(track)
             neighbour_arr = SVector{track,Int64}(neighbours_set)
    
         end
